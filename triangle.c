74b40c72106946a91f726f2b8a8af0e89c83b613
Eddy Young <jeyoung@priscimon.com>
2020-05-10 21:26:24 +0100

    Minor refactoring

--- triangle.c
+++ triangle.c
@@ -14,26 +14,25 @@ int main(int argc, char** argv)
 
     int left_pad = 2*COUNT;
 
-    for (int i=0; i<COUNT; i++)
+    for (int c=1; c<left_pad; c++)
+        printf(" ");
+
+    rows[0][0] = 1;
+    printf("  %4d\n", rows[0][0]);
+
+    for (int i=1; i<COUNT; i++)
     {
-        for (int k=1; k<left_pad; k++)
+        for (int c=1; c<left_pad; c++)
             printf(" ");
 
-        if (i == 0)
-        {
-            rows[i][0] = 1;
-            printf("  %5d\n", rows[i][0]);
-            continue;
-        }
-
         rows[i][0] = rows[i-1][0];
-        printf("%5d", rows[i][0]);
+        printf("%4d", rows[i][0]);
 
         for (int j=1; j<i+1; j++)
         {
             rows[i][j] = rows[i-1][j]+rows[i-1][j-1];
             rows[i][i+1] = rows[i-1][i];
-            printf("%5d", rows[i][j]);
+            printf("%4d", rows[i][j]);
         }
         printf("\n");
         left_pad -= 2;
